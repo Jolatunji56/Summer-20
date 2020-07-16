fd30f58d1e28c2a4557af88f484f0e466fb1f8de
Jolatunji56 <skyhighjosh03@gmail.com>
2020-07-16 12:06:26 -0400

    code for table without saving file

--- ID_BHs_Halos.py
+++ ID_BHs_Halos.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 #Loads snapshot
 s = pynbody.load ('/data/h258/stampedetesting/glenna/h258.cosmo50cmb.3072gst1bwdK1BH.000128')
@@ -11,10 +12,12 @@ s = pynbody.load ('/data/h258/stampedetesting/glenna/h258.cosmo50cmb.3072gst1bwd
 #Put the units in the right way
 s.physical_units()
 
+
 #initiates a halo catalogue object for the given snapshot
 h = s.halos()
 
 #print len(h) #__ Returns the number of halo in snapshot
+
 
 
 #Function for finding BH
@@ -24,44 +27,87 @@ def findBH(s):
     return BH
 
 def findBHhalos(s):
-    """returns the index of host halos for each BHs"""
+    """returns the indexes (ID-number) of host halos for each BHs"""
     BH = findBH(s)
     BHhalos = BH['amiga.grp']
     return BHhalos
 
+BH = findBH(s)
 BHhalos = findBHhalos(s)
 
-current_halo = np.argsort(findBHhalos)
 
+currenthalo = np.argsort(BHhalos)
+#print BHhalos[currenthalo]
 
-print current_halo
-
-
-#prints BHs 
-BH = findBH(s)
-
-
-
-#prints BHs position
-BH_position = BH['pos']
-
-
+#prints BHs id (iord is a loadable key for BHs id) 
 BH_ID = BH['iord']
+BH_ID = BH['iord']
+Iden_num = []
+BHs_ID = []
+Halo_ID = []
+Distance_cnt = []
+BHs_mass = []
 
 
-#print h_ID
+for i in currenthalo:
+
+    currenthalo = BHhalos [i]
+    #print currenthalo
+   # print (i)  #prints identity number of BH
+
+    pynbody.analysis.angmom.faceon(h[currenthalo])
+
+    BH_position = BH['pos']
+
+    BHx = BH['pos'][[i],0] #prints all x_positions for BHs
+
+    BHy = BH['pos'][[i],1] #prints all y_positions for BHs
+
+    BHz = BH['pos'][[i],2] #prints all z_positions for BHs
+
+    #locationg distance from center(cnt), using the distance formula
+    Dist_cnt = ((BHx **2) + (BHy ** 2) + (BHz ** 2)) ** 0.5 #taking center position (intial postion x1,y1,z1) to be (0,0,0)
+
+    #print (Dist_cnt)
+
+    Iden_num.append(i)
+    BHs_ID.append(BH['iord'][i])
+    Halo_ID.append(currenthalo)
+    Distance_cnt.append(Dist_cnt)
+    BHs_mass.append(BH['mass'][i])
+
+    
+
+
+data = {'Iden_num':Iden_num, 'BHs_ID':BHs_ID,'Halo_ID':Halo_ID,'Distance_cnt':Distance_cnt, 'BHs_mass':BHs_mass}
+
+#print(data)
+
+df = pd.DataFrame(data)
+
+print(df)
 
 
 
-#print (type(BH_tform))
 
 
-#print BH['mass']
 
-#print(s.families(), s.loadable_keys,())
 
-#print(BH)
 
-#print(BH_position)
 
-#print BHhalos
+
+    
+
+    
+
+
+
+
+
+
+#print (BH_position)
+#print (BH_x)
+#print Dist_cnt.type()
+
+
+
